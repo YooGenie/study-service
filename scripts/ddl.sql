b57b3af7322f37caae43468fbf3867ef8840adbf
ysun7 <ysun7@beautifulstore.org>
2021-12-18 21:23:35 +0900

    20211218 store, store_info DDL 추가

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -13,3 +13,38 @@ create table menu
     updated_at  datetime     not null,
     updated_by  varchar(50)  not null
 );
+
+
+create table store
+(
+    id varchar(20) not null,
+    password varchar(20) not null,
+    business_registration_number VARCHAR(10) not null comment '사업자번호',
+    mobile VARCHAR(11) not null comment '핸드폰번호',
+    created JSON not null,
+    updated JSON not null,
+    deleted_at datetime null,
+    constraint member_store_pk
+        primary key (id),
+    UNIQUE INDEX `uq_store_business_registration_number` (`business_registration_number`)
+);
+
+create table store_info
+(
+    id int auto_increment,
+    store_id varchar(20) not null,
+    business_registration_number VARCHAR(10) not null comment '사업자번호',
+    Business_report_no varchar(20) not null comment '영업신고증고유번호',
+    representative varchar(20) not null comment '대표자명',
+    store_name varchar(50) not null comment '영업소명칭',
+    post_no varchar(5) not null comment '우편번호',
+    address varchar(50) not null comment '주소',
+    address_detail varchar(50) not null,
+    business_type varchar(50) not null comment '영업의종류',
+    created json not null,
+    updated json null,
+    deleted_at datetime null,
+    constraint store_info_pk
+        primary key (id),
+    UNIQUE INDEX `uq_store_info_store_id` (`store_id`)
+);
