4ac284407614e9c5893237db308f9cee6c143772
ysun7 <ysun7@beautifulstore.org>
2021-12-28 00:35:08 +0900

    #20211228 Test 코드 DB 셋팅

---
+++ controller/menu_controller_test.go
@@ -0,0 +1,13 @@
+package controller
+
+import (
+	_ "github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMenuController_Get(t *testing.T) {
+	DatabaseFixture{}.setUpDefault(xormDb.Engine)
+	//given
+	//when
+	//then
+}
