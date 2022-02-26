901678f916e8fe07a14678102cdc3bd73b839101
ysun7 <ysun7@beautifulstore.org>
2022-02-26 21:34:03 +0900

    #13 데이터베이스 처리하는 부분 공부하기

--- config/database.go
+++ config/database.go
@@ -13,22 +13,24 @@ var (
 	xormDb *xorm.Engine
 )
 
+//DB 생성
 func ConfigureDatabase() DatabaseWrapper {
 	dbConnection := Config.Database.ConnectionString
 
-	xormDb, err := xorm.NewEngine(Config.Database.Driver, dbConnection)
+	xormDb, err := xorm.NewEngine(Config.Database.Driver, dbConnection) //Create Engine
 	if err != nil {
 		panic(fmt.Errorf("Database open error: error: %s \n", err))
 	} else {
-		fmt.Println("DB connected: ", Config.Database.Connection)
+		fmt.Println("DB connected: ", Config.Database.Connection) //DB 연결 성공
 	}
 
-	xormDb.SetMaxOpenConns(10)
-	xormDb.SetMaxIdleConns(5)
-	xormDb.SetConnMaxLifetime(10 * time.Minute)
+	//https://pkg.go.dev/database/sql#DB.SetMaxOpenConns 참고
+	xormDb.SetMaxOpenConns(10)  //데이터베이스에 열린 연결의 최대 수가 설정
+	xormDb.SetMaxIdleConns(5) //idle connection pool 의 최대 연결 수를 설정
+	xormDb.SetConnMaxLifetime(10 * time.Minute) //연결을 재사용할 수 있는 최대 시간을 설정
 
-	xormDb.ShowSQL(Config.Log.ShowSql)
-	xormDb.Logger().SetLevel(core.LOG_INFO)
+	xormDb.ShowSQL(Config.Log.ShowSql) //로그 수준이 INFO보다 클 경우 로거에서 SQL 문인지 여부
+	xormDb.Logger().SetLevel(core.LOG_INFO) //로거 레벨 설정
 
 	return DatabaseWrapper{xormDb}
 }
@@ -37,6 +39,7 @@ type DatabaseWrapper struct {
 	*xorm.Engine
 }
 
+//세션 만드는 함수
 func (d DatabaseWrapper) CreateSession(ctx context.Context) (*xorm.Session, context.Context) {
 	session := d.NewSession()
 
@@ -45,7 +48,7 @@ func (d DatabaseWrapper) CreateSession(ctx context.Context) (*xorm.Session, cont
 			s.SetContext(ctx)
 		}
 	}(session, ctx)
-	defer session.Close()
+	defer session.Close() //풀로부터 연결 해지
 
 	return session, context.WithValue(ctx, ContextDBKey, session)
 }
