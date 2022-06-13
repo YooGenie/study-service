9931cbcc5bb79816111721439f49f100218d0b96
ysun7 <ysun7@beautifulstore.org>
2022-06-13 19:48:56 +0900

    valitor 관련 삭제

--- config/echo.go
+++ config/echo.go
@@ -1,14 +1,13 @@
 package config
 
 import (
-	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func ConfigureEcho() *echo.Echo {
 	e := echo.New()
-	e.Validator = RegisterValidator()
+	//e.Validator = RegisterValidator()
 	e.HideBanner = true
 
 	e.Pre(middleware.RemoveTrailingSlash())
@@ -16,18 +15,4 @@ func ConfigureEcho() *echo.Echo {
 	e.Use(middleware.CORS())
 
 	return e
-}
-
-type CustomValidator struct {
-	validator *validator.Validate
-}
-
-func (cv *CustomValidator) Validate(i interface{}) (err error) {
-	return cv.validator.Struct(i)
-}
-
-func RegisterValidator() *CustomValidator {
-	customValidator := validator.New()
-
-	return &CustomValidator{validator: customValidator}
 }
